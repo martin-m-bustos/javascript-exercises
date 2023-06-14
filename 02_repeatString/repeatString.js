a4f75f6844c4dd4bb965d661db7a7d04b2a81f6e
Martin Bustos <martin.m.bustos@gmail.com>
2023-06-13 17:00:18 -0700

    Complete javascript exercises 1-3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
-};
-
+const repeatString = function(string, num) {
+    if (num > 0) {
+        let newString = string;
+        for(i = 1; i < num; i++){
+            newString = string + newString;
+        } 
+        return newString;
+    } else if (num < 0) {
+        return "ERROR";
+    } else {
+        return "";
+    }
+}
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  const myArray = str.split("");
+  let i = str.length - 1;
+  let newStr = "";
+  while (i >= 0) {
+    newStr += myArray[i];
+    i--;
+  }
+  return newStr;
 };
 
 // Do not edit below this line
