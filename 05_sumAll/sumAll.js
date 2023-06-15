3a6a21ff52f5557ff795356d886f57385bef6f3e
Martin Bustos <martin.m.bustos@gmail.com>
2023-06-14 18:23:19 -0700

    Complete sumAll javascript exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+  let sum = 0;
+  if (typeof num2 != "number" || typeof num1 != "number") {
+    sum = "ERROR";
+  } else if (num1 < 0 || num2 < 0) {
+      sum = "ERROR";
+  } else if (num2 > num1) {
+      let i = num1; let j = num2;
+      while (i <= j) {
+        sum += i;
+        i++;
+      }
+  } else if (num1 > num2) {
+      let i = num1; let j = num2;
+      while (i >= j) {
+        sum += i;
+        i--;
+      }
+  }
+  return sum;
 };
 
 // Do not edit below this line
