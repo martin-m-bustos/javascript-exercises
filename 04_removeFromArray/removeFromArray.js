abbfd33ed0ab622ac63afdefd66c3751225761a7
Martin Bustos <martin.m.bustos@gmail.com>
2023-06-14 17:29:42 -0700

    Complete removeFromArray javascript exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(arr, ...removeArgs) {
+  const myArray = [];
+    arr.forEach((item) => {
+      if(!removeArgs.includes(item)) {
+        myArray.push(item);
+      }
+    })
+  return myArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
