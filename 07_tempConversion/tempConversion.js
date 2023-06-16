5bdee1d3a533530108cd61b0d52b8ee5d53dba3d
Martin Bustos <martin.m.bustos@gmail.com>
2023-06-15 20:02:49 -0700

    Complete tempConversion javascript exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degF) {
+  let degreeC;
+  degreeC = (degF - 32) * (5/9);
+  dC = Number(degreeC.toFixed(1));
+  return dC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degC) {
+  let degreeF;
+  degreeF = degC * (9/5) + 32;
+  dF = Number(degreeF.toFixed(1));
+  return dF;
 };
 
 // Do not edit below this line
